833e67d9e23d2e1a621118458e01779395cb3a5e
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-04-19 23:15:15 -0300

    set font awesome

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'friendly_id', '~> 5.1'
 gem 'devise', '~> 4.2'
 gem 'materialize-sass'
 gem 'will_paginate', '~> 3.0', '>= 3.0.7'
+gem 'mail_form', '~> 1.5', '>= 1.5.1'
 
 
 group :development, :test do
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,2 +1,16 @@
 class ContactController < ApplicationController
+  def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(params[:contact])
+		@contact.request = request
+		if @contact.deliver
+			flash.now[:error] = nil
+		else
+			flash.now[:error] = 'Não foi possivel enviar a mensagem'
+			render :new
+ 		end
+	end
 end

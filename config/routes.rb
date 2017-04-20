6020ef8682cce30c9ae129d7f42011d462ce9aa1
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-04-19 23:45:37 -0300

    add contact form

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :users
   resources :posts
+  resources :contacts , only:[:new, :create]
   root 'welcome#index'
-
+  
   get '*path' => redirect('/')
 
 

0c067572811492d224189b86f255b6b34f55ef8d
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-05-15 19:24:17 -0300

    remove about and setup resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,49 @@
 class ResumesController < ApplicationController
+
+    def index
+        @resumes = Resume.all.order("created_at DESC").paginate(page:params[:page], per_page: 3)
+    end
+
+    def new
+        @resume = Resume.new
+    end
+
+    def create
+        @resume = Resume.new(resume_params)
+        if @resume.save
+            redirect_to @resume
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @resume.update(resume_params)
+            redirect_to @resume
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @resume.destroy
+        redirect_to root_path
+    end
+
+
+    private
+
+    def find_resume
+        @resume = Resume.friendly.find(params[:id])
+    end
+
+    def resume_params
+        params.require(:resume).permit(:title,:body, :slug)
+    end
 end

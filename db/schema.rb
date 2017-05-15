3d7b2cb147e5980e0a04d4c644e8ccbbeecf9c26
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-05-15 16:05:01 -0300

    create about for this site

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515022042) do
+ActiveRecord::Schema.define(version: 20170515190340) do
+
+  create_table "abouts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"

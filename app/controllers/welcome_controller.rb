050d0037f0add035a5567ffae40413e1afaed5b5
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-06-30 16:12:47 -0300

    finish show page for resumes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   
   
   def index
-    @services = Service.all.order("created_at DESC")
+    @services = Service.all.order("created_at DESC").limit(3)
   end
 
   

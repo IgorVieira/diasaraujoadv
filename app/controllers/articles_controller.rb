60ccef2f11ce527bf1254f5fa1043e5579bd5cf0
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-07-06 16:44:12 -0300

    I'll create a template for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,7 +52,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-      params.require(:article).permit(:title, :body, :slug)
+      params.require(:article).permit(:title, :body, :slug, :avatar)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630165950) do
+ActiveRecord::Schema.define(version: 20170706194151) do
 
   create_table "abouts", force: :cascade do |t|
     t.string   "title"
@@ -22,9 +22,13 @@ ActiveRecord::Schema.define(version: 20170630165950) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "slug"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["slug"], name: "index_articles_on_slug", unique: true
   end
 
